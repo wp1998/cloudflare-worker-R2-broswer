18b18b7727c50bd252eb0d875acf9eb0263a8413
wupeng <wp1998@qq.com>
2023-12-23 15:51:44 +0800

    支持文件删除、上传和文件夹上传

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # R2 Broswer
-a file drive power by cloudflare worker & R2 storage
+A personal network disk based on cloudflare worker and R2 object storage, used to save and manage some files online
+- Directory browsing view
+- File and folder upload
+- File deletion
+- http basic auth
 
-demo: https://r2-music.wp1998.workers.dev/
+demo: https://r2.wp1998.workers.dev/
--- src/index.js
+++ src/index.js
@@ -1,38 +1,100 @@
-import { showList } from './ui';
+import { showList } from './broswer';
+import { showLogin, validateAuthorization } from './login';
 
 export default {
 
     async fetch(request, env, ctx) {
 
-        const originResponse = await fetch(request)
-        // if status is not 404 or request path not end with '/', return origin response
-        if ((originResponse.status !== 404) || (originResponse.url.slice(-1) !== '/')) {
-            return originResponse;
-        }
         const url = new URL(request.url);
         const path = url.pathname;
-		    const uri = decodeURIComponent(url.pathname.slice(1));
+        const uri = decodeURIComponent(url.pathname.slice(1));
+        const fname = uri.substring(uri.lastIndexOf('/') + 1);
         const r2 = env.MY_BUCKET;
 
-        const index = await r2.list({
-            prefix: uri,  //URI是当前路径
-            delimiter: '/',
-            include: ['httpMetadata', 'customMetadata']
-        });
+        // 需要通过 HTTP Basic Auth，用户名和密码保存在 wrangler.toml
+        // 获取 header 验证信息
+        const authorization = request.headers.get('Authorization');
+        console.log(authorization);
+        // 用户名和密码验证
+        if (!authorization || !validateAuthorization(authorization, env)) {
+            return new Response("You need to login.", {
+                status: 401,
+                headers: {
+                    // Prompts the user for credentials.
+                    "WWW-Authenticate": 'Basic realm="my scope", charset="UTF-8"',
+                },
+            });
+        }
 
-        if (index.objects.length === 0 && index.delimitedPrefixes.length === 0) {
-            return originResponse;
+        // GET操作的两种情况
+        // - URL结尾无 "/"，单个文件对象下载
+        // - URL结尾有 "/"，显示当前目录的列表视图
+        if (request.method === 'GET') {
+
+            const originResponse = await fetch(request);
+
+            if ((originResponse.status !== 404) || (originResponse.url.slice(-1) !== '/')) {
+
+                // 下载文件
+                const object = await r2.get(uri);
+
+                if (object === null) {
+                    return new Response('Object Not Found', { status: 404 });
+                }
+
+                const headers = new Headers();
+                object.writeHttpMetadata(headers);
+                headers.set('etag', object.httpEtag);
+                headers.set('Content-Disposition', 'attachment;filename='+fname)
+
+                return new Response(object.body, {
+                    headers,
+                });
+
+            } else {
+
+                // 获取当前 prefix 目录列表。
+                // R2ListOptions ：https://developers.cloudflare.com/r2/api/workers/workers-api-reference/#r2listoptions
+                const index = await r2.list({
+                    prefix: uri,  //URI是当前路径
+                    delimiter: '/',
+                    include: ['httpMetadata', 'customMetadata']
+                })
+
+                return new Response(
+                    // 显示目录列表视图
+                    showList(index.objects, index.delimitedPrefixes, path, env),
+                    {
+                        headers: {
+                            'Content-Type': 'text/html; charset=utf-8',
+                        },
+                        status: 200,
+                    }
+                )
+            }
         };
 
-        return new Response(
-            // 显示列表视图
-            showList(index.objects, index.delimitedPrefixes, path),
-            {
-                headers: {
-                    'Content-Type': 'text/html; charset=utf-8',
-                },
-                status: 200,
-            }
-        );
+        // 文件对象上传
+        if (request.method === 'PUT') {
+            const key = uri;
+            await r2.put(key, request.body, {
+                httpMetadata: request.headers,
+            });
+            console.log(key);
+            return new Response(`Put ${key} successfully!`);
+        };
+
+        // 文件对象删除
+        if (request.method === 'DELETE') {
+
+            // const api_key = request.headers.get('API_KEY');
+            // if (api_key === env.KEY) {
+            const key = decodeURIComponent(path.substring(1));
+            await r2.delete((key));
+            console.log(key)
+            return new Response(`DELETE ${key} successfully!`)
+            
+        };
+
     }
 };
